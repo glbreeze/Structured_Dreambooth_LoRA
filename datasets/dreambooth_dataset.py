4d8c91c7c2023c2885a5f31590dc09a2410c100d
glbreeze <glbreeze@gmail.com>
2024-11-11 22:30:35 +0800

    use_diffuser

--- datasets/dreambooth_dataset.py
+++ datasets/dreambooth_dataset.py
@@ -5,6 +5,22 @@ from PIL import Image
 from pathlib import Path
 from utils import tokenize_prompt
 
+class PromptDataset(Dataset):
+    """A simple dataset to prepare the prompts to generate class images on multiple GPUs."""
+
+    def __init__(self, prompt, num_samples):
+        self.prompt = prompt
+        self.num_samples = num_samples
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, index):
+        example = {}
+        example["prompt"] = self.prompt
+        example["index"] = index
+        return example
+
 
 class DreamBoothDataset(Dataset):
     """A dataset to prepare the instance and class images with the prompts for
